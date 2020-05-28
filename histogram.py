2b5f062bec7c441c51aacf05fda152299c6b64f0
NM <35345506+NM20XX@users.noreply.github.com>
2020-05-28 14:29:29 -0700

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -1,32 +1,48 @@
-# histogram for each variables across species
+# importing modules
 
-# library used: matplotlib
-# https://matplotlib.org/3.2.1/api/_as_gen/matplotlib.pyplot.hist.html
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt # library for creating visualizations
+import seaborn as sns # library for making statistical graphics. Built on top of matplotlib
+import statistics # calculating statistics of numeric data
 
+# load the iris data to a dataframe
 iris_data = pd.read_csv('/kaggle/input/iris-flower-dataset/IRIS.csv')
 
+params = {'legend.fontsize': 9
+          , 'font.size': 16
+          , 'font.serif': ['Computer Modern Roman']
+          , 'axes.labelsize': 16
+          , 'xtick.labelsize': 15
+          , 'ytick.labelsize': 15
+         }
+plt.rcParams.update(params)
 
 iris_setosa = iris_data[iris_data['species'] == 'Iris-setosa']
 iris_virginica = iris_data[iris_data['species'] == 'Iris-virginica']
 iris_versicolor = iris_data[iris_data['species'] == 'Iris-versicolor']
 
+#species
+names = ['iris_setosa', 'iris_virginica', 'iris_versicolor']
+
 fig, (ax1, ax2, ax3) = plt.subplots(1,3,figsize=(15,5))
 
-params = {'legend.fontsize': 9, 'legend.handlelength': 1}
-plt.rcParams.update(params)
 
 ax1.hist(iris_setosa["sepal_length"], 
-          color = 'darkcyan', label=names,alpha=0.5) 
+          color = 'red', label=names, histtype = 'step') 
 
 ax2.hist(iris_virginica["sepal_length"] ,          
-          color = 'darkmagenta', label=names,alpha=0.5) 
+          color = 'green', label=names,histtype = 'step') 
 
 ax3.hist(iris_versicolor["sepal_length"],
-          color = 'darkgreen', label=names,alpha=0.5) 
+          color = 'blue', label=names,histtype = 'step') 
 
 
-ax1.set_title("Iris setosa sepal length(cm)",fontsize=15)
-ax2.set_title("Iris virginica sepal length(cm)",fontsize=15)
-ax3.set_title("Iris versicolor sepal length(cm)",fontsize=15)
+ax1.set_title("Iris setosa sepal length",fontsize=18)
+ax2.set_title("Iris virginica sepal length",fontsize=18)
+ax3.set_title("Iris versicolor sepal length",fontsize=18)
+# Set common labels
+fig.text(0.0, 0.5,  'Frequency',  ha='center', va='center', rotation='vertical')
+fig.text(0.5, 0.0, 'Sepal lenghts', ha='center', va='center', rotation='horizontal')
 
 plt.tight_layout()
