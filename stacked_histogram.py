ebd993b9b4adbf966b16ba52f7e091d545395fec
NM <35345506+NM20XX@users.noreply.github.com>
2020-05-28 15:20:06 -0700

    Create stacked_histogram.py

---
+++ stacked_histogram.py
@@ -0,0 +1,47 @@
+# stacked histogram for each variables across species
+
+# library used: matplotlib
+# https://matplotlib.org/3.2.1/api/_as_gen/matplotlib.pyplot.hist.html
+
+# importing modules
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt # library for creating visualizations
+import seaborn as sns # library for making statistical graphics. Built on top of matplotlib
+import statistics # calculating statistics of numeric data
+
+# load the iris data to a dataframe
+iris_data = pd.read_csv('/kaggle/input/iris-flower-dataset/IRIS.csv')
+
+params = {'legend.fontsize': 13
+          , 'font.size': 16
+          , 'font.serif': ['Computer Modern Roman']
+          , 'axes.labelsize': 16
+          , 'xtick.labelsize': 14
+          , 'ytick.labelsize': 14
+          , 'figure.figsize' : (8,5)
+         }
+plt.rcParams.update(params)
+
+iris_setosa = iris_data[iris_data['species'] == 'Iris-setosa']
+iris_virginica = iris_data[iris_data['species'] == 'Iris-virginica']
+iris_versicolor = iris_data[iris_data['species'] == 'Iris-versicolor']
+
+#species
+colors = ['darkcyan', 'darkmagenta', 'cornflowerblue']
+names = ['iris_setosa', 'iris_virginica', 'iris_versicolor']
+
+plt.hist([iris_setosa["sepal_length"], iris_virginica["sepal_length"], 
+          iris_versicolor["sepal_length"]],
+          stacked=True, color = colors, label=names,alpha=0.7) 
+
+
+# Plot formatting
+plt.legend()
+plt.title("Stacked histograms for sepal length",fontsize=15)
+
+plt.xlabel('Sepal length in cm')
+plt.ylabel('Frequency')
+
+plt.tight_layout()
