597a23aa6a288ec0bdd94053349fb6fc4112a0ce
NM <35345506+NM20XX@users.noreply.github.com>
2020-05-28 10:50:49 -0700

    Update and rename Box_Plot_1.py to Box_Plot_across_categories.py

--- Box_Plot_across_categories.py
+++ Box_Plot_across_categories.py
@@ -9,7 +9,6 @@ import statistics # calculating statistics of numeric data
 # load the iris data to a dataframe
 iris_data = pd.read_csv('/kaggle/input/iris-flower-dataset/IRIS.csv')
 
-# Box Plot 2:
 # Here we are comparing across levels of a categorical variable
 params = {'legend.fontsize': 9
           , 'font.size': 16
