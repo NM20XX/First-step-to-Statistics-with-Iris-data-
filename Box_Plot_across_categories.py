2ca8c50211c3141c9f780237d70119efe6eb9d9b
NM <35345506+NM20XX@users.noreply.github.com>
2020-05-31 17:28:28 -0700

    Update Box_Plot_across_categories.py

--- Box_Plot_across_categories.py
+++ Box_Plot_across_categories.py
@@ -9,7 +9,8 @@ import statistics # calculating statistics of numeric data
 # load the iris data to a dataframe
 iris_data = pd.read_csv('/kaggle/input/iris-flower-dataset/IRIS.csv')
 
-# Box Plot across categories:
+#Comparison between 3 categories for each numerical variable.
+
 params = {'legend.fontsize': 9
           , 'font.size': 16
           , 'font.serif': ['Computer Modern Roman']
@@ -22,8 +23,8 @@ plt.rcParams.update(params)
 
 fig, (ax1, ax2) = plt.subplots(1,2,figsize=(16,5))
 
-ax = sns.boxplot(x="species", y="sepal_length",data=iris_data, palette="husl", ax = ax1).set_title("Sepal length across species",fontsize=17)
-ax = sns.boxplot(x="species", y="sepal_width",data=iris_data, palette="husl", ax = ax2).set_title("Sepal width across species",fontsize=17)
+ax = sns.boxplot(x="species", y="sepal_length",data=iris_data, palette="Set3", ax = ax1).set_title("Sepal length across species",fontsize=17)
+ax = sns.boxplot(x="species", y="sepal_width",data=iris_data, palette="Set3", ax = ax2).set_title("Sepal width across species",fontsize=17)
 
 ax1.set_ylabel('')
 ax1.set_xlabel('')
@@ -37,8 +38,8 @@ ax2.set_xlabel('')
 
 fig, (ax1, ax2) = plt.subplots(1,2,figsize=(16,5))
 
-ax = sns.boxplot(x="species", y="petal_length",data=iris_data, palette="husl", ax = ax1).set_title("Petal length across species",fontsize=17)
-ax = sns.boxplot(x="species", y="petal_width",data=iris_data, palette="husl", ax = ax2).set_title("Petal width across species",fontsize=17)
+ax = sns.boxplot(x="species", y="petal_length",data=iris_data, palette="Set3", ax = ax1).set_title("Petal length across species",fontsize=17)
+ax = sns.boxplot(x="species", y="petal_width",data=iris_data, palette="Set3", ax = ax2).set_title("Petal width across species",fontsize=17)
 
 ax1.set_ylabel('')
 ax1.set_xlabel('')
